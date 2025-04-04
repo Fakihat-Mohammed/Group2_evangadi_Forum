f210d712faead8735187aee8bae1bf2e3fb024ac
Fakihat-Mohammed <fakihmoh44@gmail.com>
2025-04-04 22:01:14 +0300

    final commit

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 const db = mysql.createPool({
-  host: "localhost",
-  user: "evangadi-admin",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12771336",
   password: process.env.DB_PASSWORD,
   database: "evangadi-practice",
 });
